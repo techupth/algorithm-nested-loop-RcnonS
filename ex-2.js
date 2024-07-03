874d59b114b3d9ff124ecb758572555d396d3b37
RcnonS <ratchanon.somprasong@gmail.com>
2024-07-03 11:40:19 +0700

    ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,12 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printStar(n) {
+  let result = "";
+  let finalResult = "";
+  for (let i = 1; i <= n; i++) {
+    result += "*" + "\t";
+    finalResult += result + "\n";
+  }
+  return finalResult;
 }
+
+console.log(printStar(10));
