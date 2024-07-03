8a8da78eef70c8443153135b435859864314fd1e
RcnonS <ratchanon.somprasong@gmail.com>
2024-07-03 12:29:19 +0700

    ex-3

--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,14 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(n) {
+  let resultTable = "";
+  for (i = 1; i <= n; i++) {
+    let multiplyResult = "";
+    for (j = 1; j <= n; j++) {
+      multiplyResult += i * j + "\t";
+    }
+    resultTable += multiplyResult + "\n";
+  }
+  return resultTable;
 }
+
+console.log(multiplicationTable(5));
