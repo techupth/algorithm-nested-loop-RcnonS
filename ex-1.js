51f6d8948049799882f7e2c1140caa6cc71693cf
RcnonS <ratchanon.somprasong@gmail.com>
2024-07-03 11:33:10 +0700

    ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,14 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printPattern() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printPattern(n) {
+  let finalResult = "";
+  for (let i = 1; i <= n; i++) {
+    let result = "";
+    for (let j = 1; j <= i; j++) {
+      result += j + "\t";
+    }
+    finalResult += result + "\n";
+  }
+  return finalResult;
 }
+
+console.log(printPattern(5));
